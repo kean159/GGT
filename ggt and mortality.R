f88a1af9e39b0156fe4825ffe29e4787327500e0
coco159 <lk970902@163.com>
2023-02-01 01:30:16 +0800

    ggt

--- ggt and mortality.R
+++ ggt and mortality.R
@@ -11,9 +11,11 @@ print(tab,showAllLevels=TRUE,catDigits = 1,contDigits = 1)
 
 
 #Table 2####
+library(survival)
+library(survminer)
 #death
 table(a$death,a$logggt1)
-model1<-coxph(Surv(time,death)~logggt1+AGE+SEX,data=a) %>% summary()
+model1<-coxph(Surv(time,death)~logggt1+AGE+SEX,data=a) %>% summary() 
 model1
 model2<-coxph(Surv(time,death)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+JZY+CHD+STROKE,data=a) %>% summary()
 model2
@@ -118,6 +120,74 @@ model2<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+
 model2
 model3<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+DMTIME+KFY+YDS,data=d) %>% summary()
 model3
+
+#Figure 1 ####
+#all-cause RCS
+library(Hmisc)
+library(rms)
+dd<-datadist(a)
+options(datadist="dd")
+fit<-cph(Surv(time,death)~rcs(logggt,4)+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+DMTIME+KFY+CHD+STROKE+JZY,data =a)
+dd$limits$logggt[2]<-1.9
+fit<-update(fit)
+anova(fit)
+hr<-Predict(fit,logggt,fun = exp,ref.zero = T)
+P1<-ggplot()+geom_line(data=hr, aes(logggt,yhat),linetype="solid",size=1,alpha =1.1 ,color="red")+geom_ribbon(data=hr, aes(logggt,ymin = lower, ymax = upper),alpha = 0.1,linetype = 5,fill="white",color="black")+theme_classic()+geom_hline(yintercept=1, linetype=2,size=1)+labs( x="Log-transformed GGT levels", y="HR (95%CI) of all-cause mortality")+scale_x_continuous(limits=c(1.9,6.5), breaks=seq(2,6.5,0.5))+scale_y_continuous(limits=c(0,6), breaks=seq(0,9,1))
+P1
+#CVD RCS
+fit<-cph(Surv(time,CVDdeath)~rcs(logggt,4)+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+DMTIME+KFY+CHD+STROKE+JZY,data =a)
+dd$limits$logggt[2]<-1.9
+fit<-update(fit)
+anova(fit)
+hr<-Predict(fit,logggt,fun = exp,ref.zero = T)
+P2<-ggplot()+geom_line(data=hr, aes(logggt,yhat),linetype="solid",size=1,alpha =1.1 ,color="red")+geom_ribbon(data=hr, aes(logggt,ymin = lower, ymax = upper),alpha = 0.1,linetype = 5,fill="white",color="black")+theme_classic()+geom_hline(yintercept=1, linetype=2,size=1)+labs( x="Log-transformed GGT levels", y="HR (95%CI) of CVD mortality")+scale_x_continuous(limits=c(1.9,6.5), breaks=seq(2,6.5,0.5))+scale_y_continuous(limits=c(0,8), breaks=seq(0,9,1))
+P2
+
+#cancer RCS
+fit<-cph(Surv(time,cancerdeath)~rcs(logggt,4)+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+DMTIME+KFY+JZY,data =a)
+dd$limits$logggt[2]<-1.9
+fit<-update(fit)
+anova(fit)
+hr<-Predict(fit,logggt,fun = exp,ref.zero = T)
+P3<-ggplot()+geom_line(data=hr, aes(logggt,yhat),linetype="solid",size=1,alpha =1.1 ,color="red")+geom_ribbon(data=hr, aes(logggt,ymin = lower, ymax = upper),alpha = 0.1,linetype = 5,fill="white",color="black")+theme_classic()+geom_hline(yintercept=1, linetype=2,size=1)+labs( x="Log-transformed GGT levels", y="HR (95%CI) of cancer mortality")+scale_x_continuous(limits=c(1.9,6.5), breaks=seq(2,6.5,0.5))+scale_y_continuous(limits=c(0,8), breaks=seq(0,9,1))
+P3
+
+#Figuer 2####
+library(forestplot)
+forestplot(labeltext=as.matrix(m[,c(3)]),mean=m$v4,lower=m$v5,upper=m$v6,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
+           graph.pos=2,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
+           col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
+           title = "all-cause mortality",zero=1,xticks=c(0.5,1.0,1.5,2.0,2.5),ci.vertices=T)
+
+forestplot(labeltext=as.matrix(m[,c(8)]),mean=m$v9,lower=m$v10,upper=m$v11,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
+           graph.pos=2,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
+           col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
+           title = "CVD mortality",zero=1,xticks=c(0.5,1.0,1.5,2.0,2.5,3.0,3.5),ci.vertices=T)
+forestplot(labeltext=as.matrix(m[,c(13)]),mean=m$v14,lower=m$v15,upper=m$v16,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
+           graph.pos=2,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
+           col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
+           title = "cancer mortality",zero=1,xticks=c(0.5,1.0,1.5,2.0,2.5,3.0),ci.vertices=T)
+
+#Supplementary Figure 1####
+#GGT
+hist(a$GGT,freq =F,breaks =500,xlim =c(0,200),ylim = c(0,0.04) )
+xfit<-seq(min(a$GGT),max(a$GGT),by=0.1)
+yfit<-dnorm(xfit,mean(a$GGT),sd(a$GGT))
+lines(xfit,yfit,lwd=2)
+
+#logggt
+hist(a$logggt,freq =F,breaks = 30,ylim = c(0,0.8),main = "",xlab = "Log-transformed GGT")
+xfit<-seq(min(a$logggt),max(a$logggt),by=0.1)
+yfit<-dnorm(xfit,mean(a$logggt),sd(a$logggt))
+lines(xfit,yfit,lwd=2)
+
+
+
+#Supplementary Figure 2####
+forestplot(labeltext=as.matrix(l[,c(1:3)]),mean=l$v4,lower=l$v5,upper=l$v6,is.summary=c(T,T,T,rep(F,10),T,T,rep(F,30)),
+           graph.pos=4,graphwidth=unit(100,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(6,"mm"),
+           col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
+           title = "cancer mortality",zero=1,xticks=c(0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0),ci.vertices=T)
 
 
 

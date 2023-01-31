30b42dc48a7d6ece1dc63f463c37cf1c504e6eed
kean159 <123965933+kean159@users.noreply.github.com>
2023-02-01 02:03:55 +0800

    Update ggt and mortality.R

--- ggt and mortality.R
+++ ggt and mortality.R
@@ -1,5 +1,4 @@
-
-
+a<-read.xlsx("C:\\GGT data.xlsx")
 #Table 1####
 library(tableone)
 myVars<-c("followuptime","AGE","SEX","EDU","INCOME","SMOKE","DRINK","totall_MET","BMI","DMTIME","KFY","YDS","JZY","XZ","CHD","STROKE")
@@ -8,7 +7,6 @@ tab<-CreateTableOne(vars = myVars,data=a,factorVars = catVars)
 print(tab,showAllLevels=TRUE,catDigits = 1,contDigits = 1)
 tab<-CreateTableOne(vars = myVars,data=a,factorVars = catVars,strata="logggt1")
 print(tab,showAllLevels=TRUE,catDigits = 1,contDigits = 1)
-
 
 #Table 2####
 library(survival)
@@ -38,7 +36,6 @@ model2
 model3<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+JZY+DMTIME+KFY+YDS,data=a)%>% summary()
 model3
 
-
 #Supplementary Table 1####
 b<-a[a$time>730,]
 #death
@@ -65,7 +62,6 @@ model2<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+
 model2
 model3<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+JZY+DMTIME+KFY+YDS,data=b) %>% summary()
 model3
-
 
 #Supplementary table 2####
 c<-a[a$accidentaldeath==0,]
@@ -121,6 +117,61 @@ model2
 model3<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+DMTIME+KFY+YDS,data=d) %>% summary()
 model3
 
+#Supplementary table 4####
+e<-a[a$BMI>28,]
+#death
+table(e$death,e$logggt1)
+model1<-coxph(Surv(time,death)~logggt1+AGE+SEX,data=e) %>% summary()
+model1
+model2<-coxph(Surv(time,death)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE,data=e) %>% summary()
+model2
+model3<-coxph(Surv(time,death)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE+DMTIME+KFY+YDS,data=e) %>% summary()
+model3
+#CVD
+table(e$CVDdeath,e$logggt1)
+model1<-coxph(Surv(time,CVDdeath)~logggt1+AGE+SEX,data=e) %>% summary()
+model1
+model2<-coxph(Surv(time,CVDdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE,data=e) %>% summary()
+model2
+model3<-coxph(Surv(time,CVDdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE+DMTIME+KFY+YDS,data=e) %>% summary()
+model3
+#cancer
+table(e$cancerdeath,e$logggt1)
+model1<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX,data=e) %>% summary()
+model1
+model2<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET,data=e) %>% summary()
+model2
+model3<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+DMTIME+KFY+YDS,data=e) %>% summary()
+model3
+
+#Supplementary table 5####
+f<-read.xlsx("C:\\Supplementary table5.xlsx")
+#death
+table(f$death,f$logggt1)
+model1<-coxph(Surv(time,death)~logggt1+AGE+SEX,data=f) %>% summary()
+model1
+model2<-coxph(Surv(time,death)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE,data=f) %>% summary()
+model2
+model3<-coxph(Surv(time,death)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE+DMTIME+KFY+YDS,data=f) %>% summary()
+model3
+#CVD
+table(f$CVDdeath,f$logggt1)
+model1<-coxph(Surv(time,CVDdeath)~logggt1+AGE+SEX,data=f) %>% summary()
+model1
+model2<-coxph(Surv(time,CVDdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE,data=f) %>% summary()
+model2
+model3<-coxph(Surv(time,CVDdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+CHD+STROKE+DMTIME+KFY+YDS,data=f) %>% summary()
+model3
+#cancer
+table(f$cancerdeath,f$logggt1)
+model1<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX,data=f) %>% summary()
+model1
+model2<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET,data=f) %>% summary()
+model2
+model3<-coxph(Surv(time,cancerdeath)~logggt1+AGE+SEX+EDU+INCOME+SMOKE+DRINK+BMI+totall_MET+DMTIME+KFY+YDS,data=f) %>% summary()
+model3
+
+
 #Figure 1 ####
 #all-cause RCS
 library(Hmisc)
@@ -154,17 +205,19 @@ P3
 
 #Figuer 2####
 library(forestplot)
-forestplot(labeltext=as.matrix(m[,c(3)]),mean=m$v4,lower=m$v5,upper=m$v6,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
-           graph.pos=2,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
+m<-read.xlsx("C:\\Figure 2.xlsx")
+forestplot(labeltext=as.matrix(m[,c(1:2,6)]),mean=m$v3,lower=m$v4,upper=m$v5,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
+           graph.pos=3,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
            col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
            title = "all-cause mortality",zero=1,xticks=c(0.5,1.0,1.5,2.0,2.5),ci.vertices=T)
 
-forestplot(labeltext=as.matrix(m[,c(8)]),mean=m$v9,lower=m$v10,upper=m$v11,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
-           graph.pos=2,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
+forestplot(labeltext=as.matrix(m[,c(1:2,10)]),mean=m$v7,lower=m$v8,upper=m$v9,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
+           graph.pos=3,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
            col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
            title = "CVD mortality",zero=1,xticks=c(0.5,1.0,1.5,2.0,2.5,3.0,3.5),ci.vertices=T)
-forestplot(labeltext=as.matrix(m[,c(13)]),mean=m$v14,lower=m$v15,upper=m$v16,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
-           graph.pos=2,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
+
+forestplot(labeltext=as.matrix(m[,c(1:2,14)]),mean=m$v11,lower=m$v12,upper=m$v13,is.summary=c(T,T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10),T,rep(F,10)),
+           graph.pos=3,graphwidth=unit(70,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(5,"mm"),
            col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
            title = "cancer mortality",zero=1,xticks=c(0.5,1.0,1.5,2.0,2.5,3.0),ci.vertices=T)
 
@@ -181,11 +234,10 @@ xfit<-seq(min(a$logggt),max(a$logggt),by=0.1)
 yfit<-dnorm(xfit,mean(a$logggt),sd(a$logggt))
 lines(xfit,yfit,lwd=2)
 
-
-
 #Supplementary Figure 2####
-forestplot(labeltext=as.matrix(l[,c(1:3)]),mean=l$v4,lower=l$v5,upper=l$v6,is.summary=c(T,T,T,rep(F,10),T,T,rep(F,30)),
-           graph.pos=4,graphwidth=unit(100,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(6,"mm"),
+l<-read.xlsx("C:\\Supplementary figure 2.xlsx")
+forestplot(labeltext=as.matrix(l[,c(1:2)]),mean=l$v3,lower=l$v4,upper=l$v5,is.summary=c(T,T,T,rep(F,10),T,T,rep(F,30)),
+           graph.pos=3,graphwidth=unit(100,"mm"),boxsize=0.2,line.margin=unit(0.5,"mm"),lineheight=unit(6,"mm"),
            col=fpColors(box = "grey0",lines = "grey0",zero="black"),colgap=unit(5,"mm"),txt_gp = fpTxtGp(title=gpar(cex=1.2),ticks=gpar(cex=1)),
            title = "cancer mortality",zero=1,xticks=c(0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0),ci.vertices=T)
 
